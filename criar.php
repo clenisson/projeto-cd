9614970f10a6679531a48932590048e4cf034e07
Clenisson Calaça <clenisson.calaca@gmail.com>
2017-07-09 12:08:43 -0300

    Cadastro de curso OK

---
+++ criar.php
@@ -0,0 +1,9 @@
+<form action="salvar.php" method="post">
+    <div>
+        <label for="nome">Nome do curso:</label>
+        <input type="text" name="nome" id="nome" />
+    </div>
+	
+	<input type="submit" value="Cadastrar">
+
+</form>
